4d89aea7499d1dd66266839beeed72259929354c
Stephen Bohlen <sbohlen@gmail.com>
2013-12-04 18:05:46 -0500

    introduce ISafeSerializer interface

---
+++ Proteus.Infrastructure.Messaging.Portable/Abstractions/ISafeSerializer.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace Proteus.Infrastructure.Messaging.Portable.Abstractions
+{
+    interface ISafeSerializer
+    {
+        bool TrySerializeToStream<TSource>(TSource source, out Stream serialized);
+        bool TrySerializeToString<TSource>(TSource source, out string serialized);
+        bool TryDeserialize<TTarget>(Stream serialized, out TTarget obj);
+        bool TryDeserialize<TTarget>(string serialized, out TTarget obj); 
+    }
+}
