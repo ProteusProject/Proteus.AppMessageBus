7a629a6bc155a7bbb72ed31605db0de6b58d5993
Stephen Bohlen <sbohlen@gmail.com>
2013-12-14 09:45:26 -0500

    adding behavior-less edit and display pages

---
+++ WindowsPhone8TestingHarness/EditPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace WindowsPhone8TestingHarness
+{
+    public partial class EditPage : PhoneApplicationPage
+    {
+        public EditPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveNames_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void IncrementCounterWithAck_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void IncrementCounterWithoutAck_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
