a171502633f0e1fb9575f884e7b90f1944b38919
Stephen Bohlen <sbohlen@gmail.com>
2014-12-04 16:29:28 -0500

    replace Monitor.Enter/Exit with SemaphoreSlim (since SemaphorSlim is thread-affinity-neutral vs. Monitor's thread-affine model)

--- Proteus.Infrastructure.Messaging.Portable/FileSystemProvider.cs
+++ Proteus.Infrastructure.Messaging.Portable/FileSystemProvider.cs
@@ -8,11 +8,11 @@ namespace Proteus.Infrastructure.Messaging.Portable
 {
     public class FileSystemProvider : IFileSystemProviderAsync
     {
-        private static readonly object MutexLock = new object();
+        private static readonly SemaphoreSlim Semaphore = new SemaphoreSlim(1,1);
 
         public async Task<IFolder> GetFolderAsync(IFolder parentFolder, string folderName)
         {
-            Monitor.Enter(MutexLock);
+            await Semaphore.WaitAsync();
             try
             {
                 IFolder folder = null;
@@ -27,13 +27,13 @@ namespace Proteus.Infrastructure.Messaging.Portable
             }
             finally
             {
-                Monitor.Exit(MutexLock);
+                Semaphore.Release();
             }
         }
 
         public async Task<IFile> GetFileAsync(IFolder parentFolder, string fileName)
         {
-            Monitor.Enter(MutexLock);
+            await Semaphore.WaitAsync();
             try
             {
                 IFile file = null;
@@ -49,26 +49,26 @@ namespace Proteus.Infrastructure.Messaging.Portable
             }
             finally
             {
-                Monitor.Exit(MutexLock);
+                Semaphore.Release();
             }
         }
 
         public async Task<IFolder> CreateFolderAsync(IFolder parentFolder, string folderName, CreationCollisionOption creationCollisionOption)
         {
-            Monitor.Enter(MutexLock);
+            await Semaphore.WaitAsync();
             try
             {
                 return await parentFolder.CreateFolderAsync(folderName, creationCollisionOption);
             }
             finally
             {
-                Monitor.Exit(MutexLock);
+                Semaphore.Release();
             }
         }
 
         public async Task DeleteFolderAsync(IFolder parentFolder, string folderName)
         {
-            Monitor.Enter(MutexLock);
+            await Semaphore.WaitAsync();
             try
             {
                 var folders = await parentFolder.GetFoldersAsync();
@@ -80,26 +80,26 @@ namespace Proteus.Infrastructure.Messaging.Portable
             }
             finally
             {
-                Monitor.Exit(MutexLock);
+                Semaphore.Release();
             }
         }
 
         public async Task<IFile> CreateFileAsync(IFolder parentFolder, string filename, CreationCollisionOption creationCollisionOption)
         {
-            Monitor.Enter(MutexLock);
+            await Semaphore.WaitAsync();
             try
             {
                 return await parentFolder.CreateFileAsync(filename, creationCollisionOption);
             }
             finally
             {
-                Monitor.Exit(MutexLock);
+                Semaphore.Release();
             }
         }
 
         public async Task DeleteFileAsync(IFolder parentFolder, string filename)
         {
-            Monitor.Enter(MutexLock);
+            await Semaphore.WaitAsync();
             try
             {
                 var files = await parentFolder.GetFilesAsync();
@@ -110,33 +110,33 @@ namespace Proteus.Infrastructure.Messaging.Portable
             }
             finally
             {
-                Monitor.Exit(MutexLock);
+                Semaphore.Release();
             }
         }
 
         public async Task<string> ReadAllTextAsync(IFile file)
         {
-            Monitor.Enter(MutexLock);
+            await Semaphore.WaitAsync();
             try
             {
                 return await file.ReadAllTextAsync();
             }
             finally
             {
-                Monitor.Exit(MutexLock);
+                Semaphore.Release();
             }
         }
 
         public async Task WriteAllTextAsync(IFile file, string text)
         {
-            Monitor.Enter(MutexLock);
+            await Semaphore.WaitAsync();
             try
             {
                 await file.WriteAllTextAsync(text);
             }
             finally
             {
-                Monitor.Exit(MutexLock);
+                Semaphore.Release();
             }
         }
     }
