3154d387dd7928729eb10a541499f8a19caf1c84
sbohlen <sbohlen@gmail.com>
2016-08-07 02:19:10 -0400

    Add initial blank EditPage

--- Windows10TestingHarness/EditPage.xaml.cs
+++ Windows10TestingHarness/EditPage.xaml.cs
@@ -13,16 +13,16 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Windows10TestingHarness
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class EditPage : Page
     {
-        public MainPage()
+        public EditPage()
         {
             this.InitializeComponent();
         }
