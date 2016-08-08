a1f861b3ea7963da8d76826a5fff80d8275cc6e2
sbohlen <sbohlen@gmail.com>
2016-08-08 05:35:59 -0400

    refactoring to use common base page class

--- Windows10TestingHarness/CounterDisplayPage.xaml.cs
+++ Windows10TestingHarness/CounterDisplayPage.xaml.cs
@@ -23,81 +23,14 @@ namespace Windows10TestingHarness
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class CounterDisplayPage : Page
+    public sealed partial class CounterDisplayPage : CustomBasePage
     {
         public CounterDisplayPage()
         {
             this.InitializeComponent();
-
-            #region move to common base class
-            this.navigationHelper = new NavigationHelper(this);
-            this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
-
-            SystemNavigationManager.GetForCurrentView().BackRequested += SystemNavigationManager_BackRequested;
-            #endregion
         }
 
-        #region Move to common base class
-
-        private NavigationHelper navigationHelper;
-        private ObservableDictionary defaultViewModel = new ObservableDictionary();
-
-        private void SystemNavigationManager_BackRequested(object sender, BackRequestedEventArgs e)
-        {
-            // Get a hold of the current frame so that we can inspect the app back stack.
-
-            if (this.Frame == null)
-                return;
-
-            // Check to see if this is the top-most page on the app back stack.
-            if (this.Frame.CanGoBack)
-            {
-                // If not, set the event to handled and go back to the previous page in the app.
-                e.Handled = true;
-                this.Frame.GoBack();
-            }
-        }
-
-        /// <summary>
-        /// Gets the NavigationHelper used to aid in navigation and process lifetime management.
-        /// </summary>
-        public NavigationHelper NavigationHelper
-        {
-            get { return this.navigationHelper; }
-        }
-
-        /// <summary>
-        /// Gets the DefaultViewModel. This can be changed to a strongly typed view model.
-        /// </summary>
-        public ObservableDictionary DefaultViewModel
-        {
-            get { return this.defaultViewModel; }
-        }
-
-
-        /// <summary>
-        /// The methods provided in this section are simply used to allow
-        /// NavigationHelper to respond to the page's navigation methods.
-        /// Page specific logic should be placed in event handlers for the  
-        /// <see cref="AppUIBasics.Common.NavigationHelper.LoadState"/>
-        /// and <see cref="AppUIBasics.Common.NavigationHelper.SaveState"/>.
-        /// The navigation parameter is available in the LoadState method 
-        /// in addition to page state preserved during an earlier session.
-        /// </summary>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = this.Frame.CanGoBack
-                ? AppViewBackButtonVisibility.Visible
-                : AppViewBackButtonVisibility.Collapsed;
-            this.navigationHelper.OnNavigatedTo(e);
-        }
-
-        protected override void OnNavigatedFrom(NavigationEventArgs e)
-        {
-            this.navigationHelper.OnNavigatedFrom(e);
-        }
-
-        private void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        protected override void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
         {
             var viewModel = App.ViewModels.Get<CounterDisplayPageViewModel>();
 
@@ -105,6 +38,5 @@ namespace Windows10TestingHarness
             this.DefaultViewModel["UnacknowledgedCounter"] = null != viewModel ? viewModel.UnacknowledgedCounter : 0;
         }
 
-        #endregion
     }
 }
